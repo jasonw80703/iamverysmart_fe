c18151c12ae98d3f56ee21079e558a2f08189426
Jason Wang <jasonw80702@gmail.com>
2021-01-28 12:06:36 -0800

    connect create action

--- src/components/SentencesList.js
+++ src/components/SentencesList.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import SentenceDataService from "../services/sentence.service";
 import './SentencesList.css';
-import { Link } from "react-router-dom";
 
 export function SentencesTable({ sentences }) {
   return (
     <tbody>
       {
-        sentences.map((sentence) => {
+        sentences.slice(0).reverse().slice(0, 10).map((sentence) => {
           return (
             <tr>
               <td>{sentence.original}</td>
@@ -25,9 +24,14 @@ export default class SentencesList extends Component {
     super(props);
 
     this.fetchSentences = this.fetchSentences.bind(this);
+    this.onChangeSentence = this.onChangeSentence.bind(this);
+    this.findOrCreateSentence = this.findOrCreateSentence.bind(this);
 
     this.state = {
       sentences: [],
+      id: null,
+      original: '',
+      translated: null,
     }
   }
 
@@ -41,7 +45,33 @@ export default class SentencesList extends Component {
         this.setState({
           sentences: response.data
         });
+        // console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  onChangeSentence(e) {
+    this.setState({
+      original: e.target.value
+    });
+  }
+
+  findOrCreateSentence() {
+    var data = {
+      original: this.state.original,
+    };
+
+    SentenceDataService.create(data)
+      .then(response => {
+        this.setState({
+          id: response.data.id,
+          original: response.data.original,
+          translated: response.data.translated,
+        });
         console.log(response.data);
+        this.fetchSentences();
       })
       .catch(e => {
         console.log(e);
@@ -49,20 +79,32 @@ export default class SentencesList extends Component {
   }
 
   render() {
-    const { sentences } = this.state;
+    const { sentences, original, translated } = this.state;
 
     return (
       <div class="container">
-        <h1>iamverysmart</h1>
-        <div class="row input-row">
+        <h1 class='main-header'>iamverysmart</h1>
+        <div class="row input-row mb-5">
           <div class="col-md-8 offset-md-2">
             <div class="input-group">
-              <input type="text" class="form-control sentence-input mr-2" aria-label="text input" placeholder="Sentence to translate" maxLength='100' />
-              <button class="btn btn-outline-primary" type="button">Translate</button>
+              <input
+                type="text"
+                class="form-control sentence-input mr-2"
+                aria-label="text input"
+                placeholder="Sentence to translate"
+                value={original}
+                onChange={this.onChangeSentence}
+                maxLength='100'
+              />
+              <button class="btn btn-outline-primary" type="button" onClick={this.findOrCreateSentence}>Translate</button>
             </div>
           </div>
         </div>
-
+        {translated && (
+          <div class="row justify-content-center">
+            <h4 class="mb-5">{translated}</h4>
+          </div>
+        )}
         <div class="row">
           <table className="table table-sm table-bordered">
             <thead>
--- src/services/sentence.service.js
+++ src/services/sentence.service.js
@@ -5,16 +5,8 @@ class SentenceDataService {
     return http.get("/sentences");
   }
 
-  get(id) {
-    return http.get(`/sentences/${id}`);
-  }
-
   create(data) {
     return http.post("/sentences", data);
-  }
-
-  findByOriginal(original) {
-    return http.get(`/sentences?original=${original}`);
   }
 }
 
