d7982da6beb35d68a3d3878bf99426c0229f6f57
Jason Wang <jasonw80702@gmail.com>
2021-01-28 11:26:35 -0800

    update main view

--- src/components/SentencesList.js
+++ src/components/SentencesList.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 import SentenceDataService from "../services/sentence.service";
+import './SentencesList.css';
 import { Link } from "react-router-dom";
+
+export function SentencesTable({ sentences }) {
+  return (
+    <tbody>
+      {
+        sentences.map((sentence) => {
+          return (
+            <tr>
+              <td>{sentence.original}</td>
+              <td>{sentence.translated}</td>
+            </tr>
+          );
+        })
+      }
+    </tbody>
+  );
+}
 
 export default class SentencesList extends Component {
   constructor(props) {
@@ -31,8 +49,32 @@ export default class SentencesList extends Component {
   }
 
   render() {
+    const { sentences } = this.state;
+
     return (
-      <div>Sentences List</div>
-    )
+      <div class="container">
+        <h1>iamverysmart</h1>
+        <div class="row input-row">
+          <div class="col-md-8 offset-md-2">
+            <div class="input-group">
+              <input type="text" class="form-control sentence-input mr-2" aria-label="text input" placeholder="Sentence to translate" maxLength='100' />
+              <button class="btn btn-outline-primary" type="button">Translate</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <table className="table table-sm table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">Original</th>
+                <th scope="col">Translated</th>
+              </tr>
+            </thead>
+            <SentencesTable sentences={sentences} />
+          </table>
+        </div>
+      </div>
+    );
   }
 }
